33aca36ca5770afed6f8e9a377407ed2bbca83cd
jejpapa <ilinkyou@naver.com>
2025-02-27 19:03:51 +0900

    commit 2025.02.27

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from './pages/Index.jsx'
+import Layout from "./pages/Layout.jsx";
+import Home from "./components/Home.jsx"
+import Projects from "./components/Projects.jsx"
+import Templates from "./components/Templates.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children : [
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/projects',
+        element: <Projects/>
+      },
+      {
+        path:'/templates',
+        element: <Templates/>
+      },
+    ]
+  }
+])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+function App(){
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ frontend/src/components/Templates.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Templates = () => {
+  return (
+    <div>Templates</div>
+  )
+}
+
+export default Templates
