c9e12229df9c70da12ac47fb26cca0a521c6950d
jejpapa <ilinkyou@naver.com>
2025-03-05 14:30:59 +0900

    fix server

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,17 +1,23 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const local_api = 'http://localhost:5000'
+const local_api = 'http://localhost:5000';
+const production_api = '';
 
-const production_api = ''
+const token = localStorage.getItem('canva_token');
 
-const token = localStorage.getItem('canva_token')
+let baseURL = local_api; // Default to production
+
+if (process.env.NODE_ENV !== 'development') {
+  baseURL = production_api; // Override with local if not production
+}
+
 
 const api = axios.create({
-    baseURL: production_api,
-    headers: {
-        'Authorization': token ? `Bearer ${token}` : ""
-    },
-    withCredentials: true
-})
+  baseURL: baseURL,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : '',
+  },
+  withCredentials: true,
+});
 
-export default api
+export default api;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path')
 dotenv.config()
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'local') {
+if (process.env.NODE_ENV === 'development') {
     app.use(cors({
         origin: 'http://localhost:5173',
         credentials: true
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'local') {
 app.use('/api', require('./routes/designRoutes'))
 app.use('/api', require('./routes/authRoutes'))
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
     app.use(express.static(path.join(__dirname, "./frontend/dist")))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"))
