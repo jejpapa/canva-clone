5b9add1f862f4d8d14370bb5340704ff3516e4df
jejpapa <ilinkyou@naver.com>
2025-03-05 13:27:10 +0900

    production fix2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,16 @@
 import axios from 'axios'
+
 const local_api = 'http://localhost:5000'
-const production_api = ''
+
+const production_api = 'https://canva-clone-silk-alpha.vercel.app/'
+
 
 const token = localStorage.getItem('canva_token')
 
 const api = axios.create({
-    baseURL: local_api,
+    baseURL: process.env.NODE_ENV === 'development'?local_api:production_api,
+uction_api,
+uction_api,
     headers: {
         'Authorization': token ? `Bearer ${token}` : ""
     },
